1fa63e92734f2609d553487356c9f890d0d0423c
ivancovae <ivancovae@gmail.com>
2018-05-17 09:58:13 +0300

    Класс Сотрудника и Класс Департамента

---
+++ HW_WPF/Department.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HW_WPF
+{
+    /// <summary>
+    /// Класс карточки департамента
+    /// </summary>
+    class Department
+    {
+        private Guid _guid;
+        /// <summary>
+        /// Свойство уникального идентификатора департамента
+        /// </summary>
+        public string ID => _guid.ToString();
+
+        private string _name;
+        /// <summary>
+        /// Свойство наименования департамента
+        /// </summary>
+        public string Name => _name.ToString();
+
+        private Employee _director;
+        /// <summary>
+        /// Свойство директора департамента
+        /// </summary>
+        public Employee Director => _director;
+
+        private List<Employee> _employees;
+
+        /// <summary>
+        /// Конструктор создания карточки департамента
+        /// </summary>
+        /// <exception cref="ArgumentException">Исключение некорретного ввода Наименования, Директора</exception>
+        /// <param name="name">Наименование</param>
+        /// <param name="director">Директор</param>
+        public Department(string name, Employee director)
+        {
+            if (name == null || name == "")
+                throw new ArgumentException($"Имя не должно быть пустым", "name");
+            _name = name;
+            if (director == null)
+                throw new ArgumentException($"Для департамента должен быть задан директор", "director");
+            _director = director;
+            _employees = new List<Employee>();
+            _employees.Add(director);
+            _guid = Guid.NewGuid();
+        }
+    }
+}
---
+++ HW_WPF/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace HW_WPF
+{
+    /// <summary>
+    /// Класс карточки сотрудника
+    /// </summary>
+    class Employee
+    {
+        private Guid _guid;
+        /// <summary>
+        /// Свойство уникального идентификатора сотрудника
+        /// </summary>
+        public string ID => _guid.ToString();
+
+        private string _name;
+        /// <summary>
+        /// Свойство имени сотрудника
+        /// </summary>
+        public string Name => _name.ToString();
+
+        private string _surname;
+        /// <summary>
+        /// Свойство фамилии сотрудника
+        /// </summary>
+        public string SurName => _surname.ToString();
+        private WeakReference _department;
+        /// <summary>
+        /// Свойство департамента, в котором работает сотрудник
+        /// </summary>
+        public Department Department => (_department.Target as Department);
+
+        /// <summary>
+        /// Конструктор создания карточки сотрудника
+        /// </summary>
+        /// <exception cref="ArgumentException">Исключение некорретного ввода Имени, Фамилии, Департамента</exception>
+        /// <param name="name">Имя</param>
+        /// <param name="surname">Фамилия</param>
+        /// <param name="department">Департамент</param>
+        public Employee(string name, string surname, Department department)
+        {
+            if (surname == null || surname == "")
+                throw new ArgumentException($"Фамилия не должна быть пустой", "surname");
+            _surname = surname;
+
+            if (name == null || name == "")
+                throw new ArgumentException($"Имя не должно быть пустым", "name");
+            _name = name;
+            if (department == null)
+                throw new ArgumentException($"Сотрудник должен быть в каком-то Департаменте", "department");
+            _department = new WeakReference(department, false);
+            _guid = Guid.NewGuid();
+        }
+    }
+}
